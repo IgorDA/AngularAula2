8e42a05f14551ec69471ad997847a4d7b7c20176
IgorDA <igordiasbr@gmail.com>
2018-03-21 21:35:11 -0300

    aula 2 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppRoutingModule } from './/app-routing.module';
     AppComponent,
     HeaderComponent,
     JumbotronComponent,
-    ContatoComponent
+    ContatoComponent,
   ],
   imports: [
     BrowserModule,
